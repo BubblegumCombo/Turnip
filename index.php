834e336707066b279df727de793e457baf1fec7d
Nick Konstantino <nkonstantino91@gmail.com>
2020-06-22 14:47:28 -0700

    REVERT

--- index.php
+++ index.php
@@ -3,19 +3,8 @@
 	include("functions.php");
 
 	include("views/header.php");
-
-	if (isset($_GET['page'])) {
-		if ($_GET['page'] =='timeline') {
-
-		include ("views/timeline.php");
-
-	} else {
-
-
-		
+	
 	include("views/home.php");
-}}
-
 
 	include("views/footer.php");
 
